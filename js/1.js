9077e4e6c2a8f4c28ffe5630bcab1d41b8310c11
Ivan <ivanbakumenko89@gmail.com>
2022-05-02 19:20:05 +0300

    first commit

---
+++ js/1.js
@@ -0,0 +1,57 @@
+"use strict";
+/*
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' *9);
+
+const persone = 'Alex';
+
+const bool = true;
+
+//console.log(somthing);
+
+let und;
+
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+//console.log(obj.name)
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 6, {}, [], 'apple.bmp'];
+console.log(arr[1]); */
+
+const arr = [1, 2, 3];
+arr[10] = '3456';
+
+console.log(arr);
+
+const arrObj = {
+    a:'a',
+    1:'b',
+    2:'c',
+    abc: {
+        df:[{}, {}],
+        def: {
+
+        }
+    }
+};
+
+const b = 'b';
+arrObj.b = '1234';
+console.log(arrObj['b']);
+console.log(arrObj.b);
+
+//const obj = {a:1, b:2};
+
+const obj = {
+    'Anna': 500,
+    'Alise': 800
+};
---
+++ js/2.js
@@ -0,0 +1,10 @@
+const storeName = 'AgroMaster';
+const storeDescription = {
+  budget: 10000,
+  employees: ['Ivan', 'Yura', 'Andrey'],
+  products: {
+      ceeds: 10,
+      yad: 20,
+  },
+  open: true
+};
